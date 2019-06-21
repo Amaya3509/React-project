cc3068541d07dc5fbe86c0dd87093f390a7e8f23
dengling <474028610@qq.com>
2019-06-21 20:32:15 +0800

    删除src文件夹

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// 引入react-router-dom中BrowserRouter，重命名为Router
-import {BrowserRouter as Router} from 'react-router-dom'
-
-import App from './App'
-
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'))
--- src/pages/login/index.jsx
+++
@@ -1,4 +0,0 @@
-import React, { Component } from 'react'
-
-// 引入图片资源：在React脚手架中图片必须引入才会打包
-import
