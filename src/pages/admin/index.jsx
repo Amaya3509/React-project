80bde49cd44a2719b96c9d7a00d30d1cf68bd9b1
dengling <474028610@qq.com>
2019-06-24 03:07:06 +0800

    主界面Admin代码编写

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
 
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import LeftNav from '../../components/left-nav';
+import HeaderMain from '../../components/header-main';
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
 
 export default class Admin extends Component {
   state = {
@@ -16,20 +17,27 @@ export default class Admin extends Component {
   };
 
   render() {
+    const { collapsed } = this.state;
+
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <LeftNav collapsed={collapsed}/>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '10px 16px' }}>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+          <Header style={{ background: '#fff', padding: 0, minHeight: 100 }}>
+            <HeaderMain />
+          </Header>
+          <Content style={{ margin: '25px 16px' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              欢迎使用硅谷后台管理系统
+            </div>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: 'center' }}>
+            推荐使用谷歌浏览器，可以获得更佳页面操作体验
+          </Footer>
         </Layout>
       </Layout>
     );
   }
-
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Form, Icon, Input, Button } from 'antd'
 // 因为是分别暴露，所以这里要通过对象的解构赋值引入
 import { reqLogin } from '../../api' // index.js可以省略不写
 
 // 引入图片资源：在React脚手架中图片必须引入才会打包
-import logo from './logo.png'
+import logo from '../../assets/images/logo.png'
 
 // import语法必须写在最上面
 import './index.less'
@@ -12,14 +12,14 @@ import './index.less'
 // 缓存一下
 const Item = Form.Item
 
-class Login extends Component {
+function Login(props) {
 
 	// 点击form表单中的提交按钮，会触发form表单的onSubmit事件
-	login = (ev) => {
+	const login = (ev) => {
 		ev.preventDefault()
 
 		// validateFields方法是用来校验表单并获取表单的值
-		this.props.form.validateFields(async (error, values) => {
+		props.form.validateFields(async (error, values) => {
 			/*
 			error 代表表单校验结果
 					要么为null    代表校验通过
@@ -38,12 +38,22 @@ class Login extends Component {
         const result = await reqLogin(username, password)
 
         if(result) {
-          // 登录成功
-          this.props.history.replace('/')
+          // 登录成功 跳转至主页面Admin
+          /*
+            跳转页面的2种方法:
+              1.<Redirect to="/" /> 推荐使用在render方法中
+
+              // 推荐使用在回调函数中
+              // 会保留当前的历史记录，以便可以回退至本页面
+              2.this.props.history.push('/')
+                或
+                this.props.history.replace('/') // 不会保留当前的历史记录
+           */
+          props.history.replace('/') // 不需要再返回至登录页面了
         } else {
           // 登录失败
           // 重置密码为空
-          this.props.form.resetFields(['password'])
+          props.form.resetFields(['password'])
         }
 
 			} else {
@@ -54,7 +64,7 @@ class Login extends Component {
 	}
 
 	// 自定义校验规则函数 只要Input中一输入值就会触发validator这个函数
-	validator = (rule, value, callback) => {
+  const validator = (rule, value, callback) => {
 		// 无论如何，callback必须调用
 
 		// rule --> {validator: ƒ, field: "password", fullField: "password", type: "string"}
@@ -79,63 +89,61 @@ class Login extends Component {
 		}
 	}
 
-	render() {
-		// getFieldDecorator也是一个高阶组件
-		const {getFieldDecorator} = this.props.form
+  // getFieldDecorator也是一个高阶组件
+  const {getFieldDecorator} = props.form
 
-		return <div className="login">
-			<header className="login-header">
-				<img src={logo} alt="logo"/>
-				<h1>React项目: 后台管理系统</h1>
-			</header>
-			<section className="login-content">
-				<h2>用户登录</h2>
-				<Form onSubmit={this.login} className="login-form">
-					<Item>
-						{
-							getFieldDecorator(
-								'username',
-								{
-									rules: [
-										/*
-										// 方法1:
-										{required: true, message: '请输入用户名！'},
-										{min: 4, message: '用户名必须大于4位'},
-										{max: 15, message: '用户名必须小于15位'},
-										{pattern: /^[a-zA-Z_0-9]+$/, message: '用户名只能包含英文字母、数字和下划线'}
-										*/
-										// 方法2: 函数可以代码复用
-										{
-											validator: this.validator
-										}
-									]
-								}
-							)(
-									<Input className="login-input" prefix={<Icon type="user" />} placeholder="用户名"/>
-							)
-						}
-					</Item>
-					<Item>
-						{
-							getFieldDecorator(
-								'password',
-								{
-									rules: [
-										{
-											validator: this.validator
-										}
-									]
-								}
-							)(<Input className="login-input" prefix={<Icon type="lock" />} placeholder="密码" type="password"/>)
-						}
-					</Item>
-					<Item>
-						<Button type="primary" htmlType="submit" className="login-btn">登录</Button>
-					</Item>
-				</Form>
-			</section>
-		</div>
-	}
+  return <div className="login">
+    <header className="login-header">
+      <img src={logo} alt="logo"/>
+      <h1>React项目: 后台管理系统</h1>
+    </header>
+    <section className="login-content">
+      <h2>用户登录</h2>
+      <Form onSubmit={login} className="login-form">
+        <Item>
+          {
+            getFieldDecorator(
+              'username',
+              {
+                rules: [
+                  /*
+                  // 方法1:
+                  {required: true, message: '请输入用户名！'},
+                  {min: 4, message: '用户名必须大于4位'},
+                  {max: 15, message: '用户名必须小于15位'},
+                  {pattern: /^[a-zA-Z_0-9]+$/, message: '用户名只能包含英文字母、数字和下划线'}
+                  */
+                  // 方法2: 函数可以代码复用
+                  {
+                    validator: validator
+                  }
+                ]
+              }
+            )(
+                <Input className="login-input" prefix={<Icon type="user" />} placeholder="用户名"/>
+            )
+          }
+        </Item>
+        <Item>
+          {
+            getFieldDecorator(
+              'password',
+              {
+                rules: [
+                  {
+                    validator: validator
+                  }
+                ]
+              }
+            )(<Input className="login-input" prefix={<Icon type="lock" />} placeholder="密码" type="password"/>)
+          }
+        </Item>
+        <Item>
+          <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+        </Item>
+      </Form>
+    </section>
+  </div>
 }
 
 // 返回值是一个包装组件  <Form(Login)><Login></Login></Form(Login)>
