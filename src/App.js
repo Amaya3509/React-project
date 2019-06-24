817151b2c5d3302fa1d652be41f53f2e36ebda80
dengling <474028610@qq.com>
2019-06-24 09:03:06 +0800

    主界面Admin代码编写

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Route, Switch} from 'react-router-dom'
 import Login from './pages/login';
 import Admin from './pages/admin'
 
-// 没有state，也没用上生命周期函数就可以定义成工厂函数组件(直接return，就不再render了)
+// 没有state，也没用上除constructor/render以外的生命周期函数就可以定义成工厂函数组件(直接return，就不再render了)
 export default function App() {
 	// Switch 切换。 里面的组件有且只有一个生效（从上到下依次匹配）
 	return <Switch>
